baac92a425aa7f1024851baeabab66cddfa922d7
Shafanda Nabil Sembodo <shafanda.nabil.s@gmail.com>
2024-06-05 13:38:33 +0700

    init

---
+++ src/main.py
@@ -0,0 +1,28 @@
+# Minta input user
+nApel = int(input('Masukkan jumlah Apel: '))
+nJeruk = int(input('Masukkan jumlah Jeruk: '))
+nAnggur = int(input('Masukkan jumlah Anggur: '))
+
+# Definisikan harga buah
+hargaApel = 10000
+hargaJeruk = 15000
+hargaAnggur = 20000
+
+# Hitung total harga per buah
+totalHargaApel = nApel * hargaApel
+totalHargaJeruk = nJeruk * hargaJeruk
+totalHargaAnggur = nAnggur * hargaAnggur
+
+# Hitung total harga belanja
+totalBelanja = totalHargaApel + totalHargaJeruk + totalHargaAnggur
+
+# Tampilkan rincian belanja
+print(f'''
+Detail Belanja
+      
+Apel: {nApel} x {hargaApel} = {totalHargaApel}
+Jeruk: {nJeruk} x {hargaJeruk} = {totalHargaJeruk}
+Anggur: {nAnggur} x {hargaAnggur} = {totalHargaAnggur}
+
+Total: {totalBelanja}
+''')
