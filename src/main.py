0ddfaea61991a59ba8f2a9b0e16783fc601ff8b7
Shafanda Nabil Sembodo <shafanda.nabil.s@gmail.com>
2024-06-06 13:33:38 +0700

    Tambah fitur pembayaran

--- src/main.py
+++ src/main.py
@@ -1,14 +1,47 @@
 print('Selamat Datang di Pasar Buah!')
 
-# Minta input user
-nApel = int(input('Masukkan jumlah Apel: '))
-nJeruk = int(input('Masukkan jumlah Jeruk: '))
-nAnggur = int(input('Masukkan jumlah Anggur: '))
+# Definisikan stock buah
+stockApel = 10
+stockJeruk = 8
+stockAnggur = 15
 
 # Definisikan harga buah
 hargaApel = 10000
 hargaJeruk = 15000
 hargaAnggur = 20000
+
+# Minta input jumlah buah apel
+while True:
+    # Input jumlah
+    nApel = int(input('Masukkan jumlah Apel: '))
+    # Membandingkan antara permintaan dengan stock
+    if nApel > stockApel:
+        print(f'Jumlah terlalu banyak, stock tersisa {stockApel} buah')
+        continue
+    # Berhenti minta input, ketika permintaan terpenuhi
+    break
+
+# Minta input jumlah buah Jeruk
+while True:
+    # Input jumlah
+    nJeruk = int(input('Masukkan jumlah Jeruk: '))
+    # Membandingkan antara permintaan dengan stock
+    if nJeruk > stockJeruk:
+        print(f'Jumlah terlalu banyak, stock tersisa {stockJeruk} buah')
+        continue
+    # Berhenti minta input, ketika permintaan terpenuhi
+    break
+
+# Minta input jumlah buah Anggur
+while True:
+    # Input jumlah
+    nAnggur = int(input('Masukkan jumlah Anggur: '))
+    # Membandingkan antara permintaan dengan stock
+    if nAnggur > stockAnggur:
+        print(f'Jumlah terlalu banyak, stock tersisa {stockAnggur} buah')
+        continue
+    # Berhenti minta input, ketika permintaan terpenuhi
+    break
 
 # Hitung total harga per buah
 totalHargaApel = nApel * hargaApel
@@ -28,3 +61,21 @@ Anggur: {nAnggur} x {hargaAnggur} = {totalHargaAnggur}
 
 Total: {totalBelanja}
 ''')
+
+# Proses pembayaran
+while True:
+    # Input jumlah uang
+    bayar = int(input('Silahkan masukkan uang Anda: '))
+
+    # Hitung selisih antara bayar dengan total
+    selisih = totalBelanja - bayar
+
+    # Bandingkan antara uang dengan total harga
+    if selisih > 0: 
+        print(f'Uang Anda kurang sebesar Rp.{selisih}')
+        continue
+    
+    # Ucapkan terima kasih ketika selesai pembayaran
+    else:
+        print(f'''Terimakasih. Uang kembalian Anda: {abs(selisih)}''')
+        break
